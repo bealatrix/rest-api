bca8ebace82b445b5a64e8d4fe8c365f8cbc8232
Beatriz Vitoria <beatrizvitoria519@gmail.com>
2023-04-10 10:06:23 -0400

    Refatoring

--- controllers/imagens-controller.js
+++ controllers/imagens-controller.js
@@ -4,23 +4,23 @@ exports.deleteImagem = async (req, res, next) => {
     try {
 
         const query = 'DELETE FROM imagens_produtos WHERE id_imagem = ?';
-        await mysql.execute(query, [req.body.id_imagem]);
+        await mysql.execute(query, [req.params.id_imagem]);
 
         const response = {
             mensagem: 'Imagem removida com sucesso',
             request: {
                 tipo: 'POST',
                 descricao: 'Insere uma imagem',
-                url: 'http://localhost:3000/produtos' + req.body.id_produto + '/imagem',
+                url: 'http://localhost:3000/produtos/' + req.body.id_produto + '/imagem',
                 body: {
                     id_produto: 'Number',
-                    imagem_produto: 'File',
+                    imagem_produto: 'File'
                 }
             }
         }
-
         return res.status(202).send(response);
     } catch (error) {
+        console.error(error)
         return res.status(500).send({ error: error })
     }
 };
