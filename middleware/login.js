6f7389893fd88ab5a7a14815c09806bb8d78dba1
Beatriz Vitoria <beatrizvitoria519@gmail.com>
2023-04-15 12:04:07 -0400

    correções

--- middleware/login.js
+++ middleware/login.js
@@ -5,7 +5,7 @@ exports.required = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, process.env.JWT_KEY);
-        req.usuario = decode;
+        req.user = decode;
         next();
     
     }   
